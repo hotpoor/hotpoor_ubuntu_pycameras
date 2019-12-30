ea61f08d5d8b1e3245ad36d2f33cc7eafdcb7d45
hotpoor <liwei.xia0223@gmail.com>
2019-12-30 20:31:42 +0800

    Update

--- setup.sh
+++ setup.sh
@@ -5,3 +5,12 @@ python3 -m pip install idna
 python3 -m pip install numpy
 python3 -m pip install requests
 python3 -m pip install urllib3
+
+sudo apt-get install build-essential git cmake pkg-config -y
+sudo apt-get install libjpeg8-dev -y
+sudo apt-get install libtiff5-dev -y
+sudo apt-get install libjasper-dev -y
+sudo apt-get install libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libgtk2.0-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
