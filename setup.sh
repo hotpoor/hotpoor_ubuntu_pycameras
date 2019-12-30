8666a86069105b035cff15acc770d6fd4defa31a
hotpoor <liwei.xia0223@gmail.com>
2019-12-30 13:58:52 +0800

    update

---
+++ setup.sh
@@ -0,0 +1,7 @@
+python3 -m pip install certifi
+python3 -m pip install chardet
+python3 -m pip install opencv-python
+python3 -m pip install idna
+python3 -m pip install numpy
+python3 -m pip install requests
+python3 -m pip install urllib3
